428dbb0e7e6f67e8267d9c8e678ae87e01f3a4ba
makovec <makovecbarbara1@gmail.com>
2024-03-27 18:11:12 +0100

    risanje maske + patient_0 podatki

--- shape-convert.py
+++ shape-convert.py
@@ -2,9 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import nibabel as nib
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
 
-input_folder = r'images/patient_0'
+input_folder = r'patient_0'
 
 pet_path = os.path.join(input_folder, 'PET.nii.gz')
 ct_path = os.path.join(input_folder, 'CT.nii.gz')
@@ -15,3 +17,36 @@ ct_img = nib.load(ct_path).get_fdata()
 mask_img = nib.load(mask_path).get_fdata()
 
 print(pet_img.shape)
+
+
+def normalize(input_image, input_mask):
+    input_image = tf.cast(input_image, tf.float32) / 255.0
+    input_mask -= 1
+    return input_image, input_mask
+
+
+input_image, input_mask = normalize(pet_img, mask_img)
+
+print(np.min(input_image), np.max(input_image)) # vrednosti so med 0 in 1
+print(np.min(ct_img), np.max(ct_img))
+print(np.min(mask_img), np.max(mask_img)) # vrednosti so med -1 in 0, gl # Risanje maske
+
+
+
+"""
+# Risanje slik maske
+
+def plot_mask_slice(mask_img):
+    count = 0
+    for i in range(mask_img.shape[2]):
+        slice = mask_img[:, :, i]
+        if (np.min(slice) == -1) and (np.max(slice) == 0) and count == 20:
+            plt.imshow(slice, cmap='gray')
+            plt.show()
+            break
+        elif (np.min(slice) == -1) and (np.max(slice) == 0):
+            count += 1
+            continue
+
+plot_mask_slice(mask_img)
+"""
